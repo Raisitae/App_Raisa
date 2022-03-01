9e2807d59a4063532d452ba939dac73c7a200c58
Raisa <raisaerlich@gmail.com>
2022-03-01 18:45:30 -0300

    icono carrito y props agregados

---
+++ src/components/ItemListContainer.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ItemListContainer = ({ persona }) => {
+  return (
+    <div>
+      <h1> Hola {persona} ! </h1>
+    </div>
+  );
+};
+
+export default ItemListContainer;
